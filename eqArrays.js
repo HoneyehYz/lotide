68002286f73ff1a75d34f981a4dd9fec2e25ae45
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-15 23:29:59 +0000

    EqualArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,42 +1,46 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(result) {
-  if (result == true) {
-    console.log(`✅✅✅Assertion Passed`);
+  //console.log(result);
+  if (result[2]) {
+    console.log(`✅✅✅Assertion Passed ${result[0]} = ${result[1]}`);
   } else {
-    console.log(`🛑🛑🛑Assertion failed`);
+    console.log(`🛑🛑🛑Assertion failed ${result[0]} != ${result[1]}`);
   }
 
 };
 
 
 const eqArrays = function(arrOne, arrTwo) {
+  let output = [];
+  output[0] = arrOne;
+  output[1] = arrTwo;
+  
   if(arrOne.length == arrTwo.length){
-    let result = true ;
+    output[2] = true ;
     for(var i = 0; i < arrOne.length; i++){
       if(arrOne[i] !== arrTwo[i]) {
-        result = false;
+        output[2] = false;
       } 
     
     }
-    if(result == true)
+    if(output[2] == true)
     {
       //console.log("Equal");
-      return true;
+      return output;
     } else {
       //console.log("Not Equal");
-      return false;
+      return output;
     }
   } else {
-    //console.log("Not Equal");
-    return false;
+    output[2] = false
+    return output;
   }
-
-  return arrOne,arrTwo;
+return "hi";
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2,3]));
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]));
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 //assertEqual(1, 1);
 //assertEqual("bootcamp", "Bootcamp");
