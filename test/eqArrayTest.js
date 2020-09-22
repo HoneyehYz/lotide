4221bce281c6c6ce98803a511de3e7b691357207
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-22 01:02:31 +0000

    eqArrays Unit test

---
+++ test/eqArrayTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+eqArrays([1, 2, 3], [1, 2,3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
+eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+eqArrays(["1", "2", "3"], ["1", "2", 3]);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
