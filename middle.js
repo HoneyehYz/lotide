0e872c9f6eef6ae2ca16c12d6cdc6be893cc81a8
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-22 02:22:56 +0000

    Updated middle

--- middle.js
+++ middle.js
@@ -1,78 +1,78 @@
-const eqArrays = function(arrOne, arrTwo) {
-  let output = [];
-  output[0] = arrOne;
-  output[1] = arrTwo;
+// const eqArrays = function(arrOne, arrTwo) {
+//   let output = [];
+//   output[0] = arrOne;
+//   output[1] = arrTwo;
   
-  if(arrOne.length == arrTwo.length){
-    output[2] = true ;
-    for(var i = 0; i < arrOne.length; i++){
-      if(arrOne[i] !== arrTwo[i]) {
-        output[2] = false;
-      } 
+//   if(arrOne.length == arrTwo.length){
+//     output[2] = true ;
+//     for(var i = 0; i < arrOne.length; i++){
+//       if(arrOne[i] !== arrTwo[i]) {
+//         output[2] = false;
+//       } 
     
-    }
-    if(output[2] == true)
-    {
-      console.log("Equal");
-      return output;
-    } else {
-      console.log("Not Equal");
-      return output;
-    }
-  } else {
-    console.log("Not Equal");
-    output[2] = false
-    return output;
-  }
-return "hi";
-}
+//     }
+//     if(output[2] == true)
+//     {
+//       console.log("Equal");
+//       return output;
+//     } else {
+//       console.log("Not Equal");
+//       return output;
+//     }
+//   } else {
+//     console.log("Not Equal");
+//     output[2] = false
+//     return output;
+//   }
+// return "hi";
+// }
 
-//eqArrays([1, 2, 3], [1, 2,3]);
-//eqArrays([1, 2, 3], [3, 2, 1]);
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-//eqArrays(["1", "2", "3"], ["1", "2", 3]);
-
-
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(arr) {
-  //console.log(result);
-  if (arr) {
-    console.log(`✅✅✅Assertion Passed`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed`);
-  }
-
-};
+// //eqArrays([1, 2, 3], [1, 2,3]);
+// //eqArrays([1, 2, 3], [3, 2, 1]);
+// //eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+// //eqArrays(["1", "2", "3"], ["1", "2", 3]);
 
 
-const assertArraysEqual = function(arrOne, arrTwo) {
-  let output = [];
-  output[0] = arrOne;
-  output[1] = arrTwo;
+
+// // FUNCTION IMPLEMENTATION
+// const assertEqual = function(arr) {
+//   //console.log(result);
+//   if (arr) {
+//     console.log(`✅✅✅Assertion Passed`);
+//   } else {
+//     console.log(`🛑🛑🛑Assertion failed`);
+//   }
+
+// };
+
+
+// const assertArraysEqual = function(arrOne, arrTwo) {
+//   let output = [];
+//   output[0] = arrOne;
+//   output[1] = arrTwo;
   
-  if(arrOne.length == arrTwo.length){
-    output[2] = true ;
-    for(var i = 0; i < arrOne.length; i++){
-      if(arrOne[i] !== arrTwo[i]) {
-        output[2] = false;
-      } 
+//   if(arrOne.length == arrTwo.length){
+//     output[2] = true ;
+//     for(var i = 0; i < arrOne.length; i++){
+//       if(arrOne[i] !== arrTwo[i]) {
+//         output[2] = false;
+//       } 
     
-    }
-    if(output[2] == true)
-    {
-      //console.log("Equal");
-      return output;
-    } else {
-      //console.log("Not Equal");
-      return output;
-    }
-  } else {
-    output[2] = false
-    return output;
-  }
-return "hi";
-}
+//     }
+//     if(output[2] == true)
+//     {
+//       //console.log("Equal");
+//       return output;
+//     } else {
+//       //console.log("Not Equal");
+//       return output;
+//     }
+//   } else {
+//     output[2] = false
+//     return output;
+//   }
+// return "hi";
+// }
 
 //assertEqual(assertArraysEqual([1, 2, 3], [1, 2,3]));
 //assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]));
@@ -94,10 +94,4 @@ const middle = function(arr) {
 
 }
 
-assertEqual(middle([1])); // => []
-assertEqual(middle([1, 2])); // => []
-
-assertEqual(middle([1, 2, 3])); // => [2]
-assertEqual(middle([1, 2, 3, 4, 5])); // => [3]
-assertEqual(middle([1, 2, 3, 4])); // => [2, 3]
-assertEqual(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
