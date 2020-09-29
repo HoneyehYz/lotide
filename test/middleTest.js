523bb82e1cc6b9fc793e6d4eb7810c103d42e2c2
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-29 03:58:02 +0000

    Refactor with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,23 @@
 
-const eqArrays = require('../eqArrays');
-const middle = require('../middle');
-eqArrays(middle([1]),[]); // => []
-eqArrays(middle([1, 2]),[]); // => []
-eqArrays(middle([1, 2, 3]),[2]); // => [2]
-eqArrays(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-eqArrays(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-eqArrays(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
-eqArrays(middle([1, 2, 3, 4, 5]),[3]);
+// const eqArrays = require('../eqArrays');
+// const middle = require('../middle');
+// eqArrays(middle([1]),[]); // => []
+// eqArrays(middle([1, 2]),[]); // => []
+// eqArrays(middle([1, 2, 3]),[2]); // => [2]
+// eqArrays(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+// eqArrays(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
+// eqArrays(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
+// eqArrays(middle([1, 2, 3, 4, 5]),[3]);
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3,4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,20 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
 
 
-assertEqual([5,6,7],tail([5,6,7]));
+// assertEqual([5,6,7],tail([5,6,7]));
+
+
+
+const assert = require('chai').assert;
+const tail = require('../tail');;
+
+describe("#tail", () => {
+  
+  it("returns [ 2, 3 ] for [ 1, 2, 3 ]", () => {
+    assert.strictEqual(tail([ 1, 2, 3 ]), [ 2, 3 ]);
+  });
+
+});
+
+
